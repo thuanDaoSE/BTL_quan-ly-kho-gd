74409a4e65d2923dd28e25eb4346e1dab9f39048
đào thuận <td2005dataeg@gmail.com>
2026-03-12 20:42:45 +0700

    Khoi tao du an Quan Ly Kho

---
+++ src/main/java/com/example/demo/QuanLyKhoGdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyKhoGdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyKhoGdApplication.class, args);
+	}
+
+}
